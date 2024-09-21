655e5926241ad29b268aaf4019a917875f7b1305
mubashirbappi <mubashirbappi@gmail.com>
2024-09-22 02:18:03 +0600

    udate

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 
 const selectedSeat = document.getElementById('Selected-seat'); 
 const Nodata = document.getElementById('no-data')
+const totalCountbook = document.getElementById('count-seat')
+let selectedSeatIteems = []
 function handleSelectSeat(event) {
     console.log(event.innerText); 
+    event.classList.add('bg-primary')
+    event.classList.add('text-white')
+    selectedSeatIteems.push(event.innerText)
+    totalCountbook.innerText = selectedSeatIteems.length
+   
 
    Nodata.classList.add('hidden')
     const p =document.createElement('p')
@@ -12,4 +19,5 @@ function handleSelectSeat(event) {
         <span>550</span>
     </li>`;
     selectedSeat.appendChild(p)
+    console.log(selectedSeatIteems)
 }
