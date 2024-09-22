113333754c0d64fad2c1a5a4f1e487341da2ac03
mubashirbappi <mubashirbappi@gmail.com>
2024-09-22 16:56:52 +0600

    update data

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const selectedSeat = document.getElementById('Selected-seat');
 const Nodata = document.getElementById('no-data')
 const totalCountbook = document.getElementById('count-seat')
 let selectedSeatIteems = []
+let totalPrice = 0;
+const totalseatPrice = document.getElementById('total-price')
+const couponInput = document.getElementById('coupon-input')
+const couponBtn = document.getElementById('coupon-btn')
 function handleSelectSeat(event) {
-    console.log(event.innerText); 
+    const value = event.innerText;
+    if(selectedSeatIteems.includes(value)){
+        return alert("seat already added")
+    }
+   else if(selectedSeatIteems.length < 4){
+    
     event.classList.add('bg-primary')
     event.classList.add('text-white')
     selectedSeatIteems.push(event.innerText)
@@ -19,5 +28,40 @@ function handleSelectSeat(event) {
         <span>550</span>
     </li>`;
     selectedSeat.appendChild(p)
-    console.log(selectedSeatIteems)
+    
+
+totalPrice +=550;
+totalseatPrice.innerText = totalPrice.toFixed(2);
+
+if(selectedSeatIteems.length > 3){
+    couponInput.removeAttribute('disabled')
+    couponBtn.removeAttribute('disabled')
+    
 }
+
+   }
+   else{
+    return alert("you reached max seat")
+   }
+
+}
+
+// coupon
+document.getElementById('coupon-btn').addEventListener("click",function(e){
+    e.preventDefault()
+    const couponApply = document.getElementById('coupon-input').value
+    if( couponApply === "NEW50"){
+        const total = totalPrice /2
+       
+        const discountPrice = document.getElementById('coupon-total')
+        discountPrice.innerText = total
+
+    }
+    else if(couponApply ==='COUPLE2'){
+        const total = totalPrice - (totalPrice*0.30)
+        console.log(total)
+        const discountPrice = document.getElementById('coupon-total')
+        discountPrice.innerText = total
+    }
+
+})
